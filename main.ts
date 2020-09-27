1318105fc10940ae2cb993e2679189a8037821a6
xd-Orange <71978398+xd-Orange@users.noreply.github.com>
2020-09-27 10:27:39 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+// When the program starts, the LED grid will count down from 5 to 1 and show "Happy New Year!" across the screen at the end.
+// show number 5
+basic.showNumber(5)
+// pause for 1000 ms or 1 second
+basic.pause(100)
+// show number 4
+basic.showNumber(4)
+// pause for 1000 ms or 1 second
+basic.pause(100)
+// show number 3
+basic.showNumber(3)
+// pause for 1000 ms or 1 second
+basic.pause(100)
+// show number 2
+basic.showNumber(2)
+// pause for 1000 ms or 1 second
+basic.pause(100)
+// show number 1
+basic.showNumber(1)
+// pause for 1000 ms or 1 second
+basic.pause(100)
+basic.showString("Happy New Year!")
